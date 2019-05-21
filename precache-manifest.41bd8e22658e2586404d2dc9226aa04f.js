abf577a21511229e6de5adb8a4d62cfaf59ba771
haeke <riverae2@student.wpunj.edu>
2019-05-21 12:21:25 -0400

    Updates

---
+++ precache-manifest.41bd8e22658e2586404d2dc9226aa04f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be77f830e406a13c31e8ea042ea9706a",
+    "url": "/ping-coming-soon/index.html"
+  },
+  {
+    "revision": "e82f01cadeb86a2f47bc",
+    "url": "/ping-coming-soon/static/css/main.fe270b43.chunk.css"
+  },
+  {
+    "revision": "576c2d73b90e0df63a01",
+    "url": "/ping-coming-soon/static/js/2.6f6949b4.chunk.js"
+  },
+  {
+    "revision": "e82f01cadeb86a2f47bc",
+    "url": "/ping-coming-soon/static/js/main.39907922.chunk.js"
+  },
+  {
+    "revision": "9c09f6a47418d0e9dc56",
+    "url": "/ping-coming-soon/static/js/runtime~main.65c7f79f.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/ping-coming-soon/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "ddf22f0b5d6907a8b879a12905d87491",
+    "url": "/ping-coming-soon/static/media/logo.ddf22f0b.svg"
+  }
+]);
