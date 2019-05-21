89db3c383c672c942dc0bbe6bcf1d3bdf6238ead
Edwin Rivera <waehake@gmail.com>
2019-05-21 09:37:01 -0400

    added the launch header and launch sub header with styling, changed the flex direction of the headerContainer div to column.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,6 +8,10 @@ const Header = () => {
   return (
     <section className="headerContainer">
       <img className="headerLogo" src={logo} alt="ping logo" />
+      <h1 className="launchHeader">
+        We are launching <span className="launchSpan">soon!</span>
+      </h1>
+      <h3 className="subHeader">Subscribe and get notified</h3>
     </section>
   );
 };
